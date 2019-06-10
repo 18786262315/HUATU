c0004f2b5d1d757619d29033dd86ff4750bd691a
cc <843092012@qq.com>
2019-06-10 19:48:18 +0800

    第一次提交

---
+++ comm/common.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+
+# from tkinter import *
+# import os, subprocess, time, re
+
+
+def usr_sign_up():
+    # 用户注册窗口
+    def sign_to_Hongwei_Website():
+        # 以下三行就是获取我们注册时所输入的信息
+        np = new_pwd.get()
+        npf = new_pwd_confirm.get()
+        nn = new_name.get()
+ 
+        # 这里是打开我们记录数据的文件，将注册信息读出
+        with open('usrs_info.pickle', 'rb') as usr_file:
+            exist_usr_info = pickle.load(usr_file)
+        # 这里就是判断，如果两次密码输入不一致，则提示Error, Password and confirm password must be the same!
+        if np != npf:
+            tkinter.messagebox.showerror('Error', 'Password and confirm password must be the same!')
+ 
+        # 如果用户名已经在我们的数据文件中，则提示Error, The user has already signed up!
+        elif nn in exist_usr_info:
+            tkinter.messagebox.showerror('Error', 'The user has already signed up!')
+ 
+        # 最后如果输入无以上错误，则将注册输入的信息记录到文件当中，并提示注册成功Welcome！,You have successfully signed up!，然后销毁窗口。
+        else:
+            exist_usr_info[nn] = np
+            with open('usrs_info.pickle', 'wb') as usr_file:
+                pickle.dump(exist_usr_info, usr_file)
+            tkinter.messagebox.showinfo('Welcome', 'You have successfully signed up!')
+            # 然后销毁窗口。
+            window_sign_up.destroy()
+    window_sign_up = tk.Toplevel()
+    window_sign_up.geometry('300x200')
+    window_sign_up.title('Sign up window')
+
+    new_name = tk.StringVar()  # 将输入的注册名赋值给变量
+    new_name.set('example@python.com')  # 将最初显示定为'example@python.com'
+    tk.Label(window_sign_up, text='User name: ').place(x=10, y=10)  # 将`User name:`放置在坐标（10,10）。
+    entry_new_name = tk.Entry(window_sign_up, textvariable=new_name)  # 创建一个注册名的`entry`，变量为`new_name`
+    entry_new_name.place(x=130, y=10)  # `entry`放置在坐标（150,10）.
+
+    new_pwd = tk.StringVar()
+    tk.Label(window_sign_up, text='Password: ').place(x=10, y=50)
+    entry_usr_pwd = tk.Entry(window_sign_up, textvariable=new_pwd, show='*')
+    entry_usr_pwd.place(x=130, y=50)
+
+    new_pwd_confirm = tk.StringVar()
+    tk.Label(window_sign_up, text='Confirm password: ').place(x=10, y=90)
+    entry_usr_pwd_confirm = tk.Entry(window_sign_up, textvariable=new_pwd_confirm, show='*')
+    entry_usr_pwd_confirm.place(x=130, y=90)
+
+    # 下面的 sign_to_Hongwei_Website
+    btn_comfirm_sign_up = tk.Button(window_sign_up, text='Sign up', command=sign_to_Hongwei_Website)
+    btn_comfirm_sign_up.place(x=180, y=120)
+
+
